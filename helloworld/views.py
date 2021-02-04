285a0e99c9c7f91c5882dc62b56602cd3fc09fde
Aleksey Burov <allburov@gmail.com>
2021-02-04 16:17:53 +0700

    Error

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
 
 def index(request):
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        users = User.objects.all()
+    except:
+        return HttpResponse("Oh, ah, you don't have a DB!")
+    
     return HttpResponse("Hello, world!")
